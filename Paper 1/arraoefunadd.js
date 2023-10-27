820b5c586d1548552df081ef8db1cab5296a2eb1
pranjal444 <pranjalpagar02@gmail.com>
2023-10-27 16:27:50 +0530

    HOF

---
+++ Paper 1/arraoefunadd.js
@@ -0,0 +1,11 @@
+const addNumbers = (a, b) => a + b;
+console.log(addNumbers(1,2));
+
+const subNumbers=(a,b)=>a-b;
+console.log(subNumbers(80,70));
+
+const mulNumbers =(a,b)=>a*b;
+console.log(mulNumbers(8,9));
+
+const divNumbers= (a,b)=>a/b;
+console.log(divNumbers(25,5));
